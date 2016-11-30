24e720a955f2904b363a7cbddcda8ab714e2acfe
Fotoblysk <fotoblysk@fejm.pl>
2016-11-30 22:38:20 +0100

    Added basic test routine, and 2 simple tests

--- Cap-chat_core/CapChat.cpp
+++ Cap-chat_core/CapChat.cpp
@@ -12,27 +12,4 @@
 
 CapChat::CapChat() {
     std::cout << "hello Cap-chat" << std::endl;
-    sf::RenderWindow window;
-
-
-
-    window.getDefaultView();
-    window.create(sf::VideoMode(800, 600), "SFML window");
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // Request for closing the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.draw(sf::CircleShape(220));
-        sf::Music music;
-        if (!music.openFromFile("music.ogg"))
-
-        music.play();
-
-        window.display();
-        window.clear();
-    }
 }
--- Cap-chat_tests/basic_test/basic_check.cpp
+++ Cap-chat_tests/basic_test/basic_check.cpp
@@ -6,3 +6,6 @@
 TEST(basic_check, test_neq) {
 EXPECT_NE(1, 0);
 }
+TEST(basic_check, test_eq) {
+EXPECT_EQ(0, 0);
+}
