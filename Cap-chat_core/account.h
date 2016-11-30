8080847f2ddde99ef9c601d6da5e206559710a22
juzeg <witold.kaptur@gmail.com>
2016-12-01 00:19:31 +0100

    first commit

---
+++ Cap-chat_core/account.h
@@ -0,0 +1,8 @@
+//
+// Created by juzeg on 30.11.16.
+//
+
+#ifndef CAP_CHAT_ACCOUNT_H
+#define CAP_CHAT_ACCOUNT_H
+
+#endif //CAP_CHAT_ACCOUNT_H
---
+++ Cap-chat_core/command.h
@@ -0,0 +1,16 @@
+//
+// Created by juzeg on 30.11.16.
+//
+
+#ifndef CAP_CHAT_COMMAND_H
+#define CAP_CHAT_COMMAND_H
+
+
+
+class command {
+
+};
+
+
+
+#endif //CAP_CHAT_COMMAND_H
